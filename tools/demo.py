b4a074a53f60805d67009b99abe1acf24f0b441d
al_lu <alu@mission-embedded.com>
2022-12-19 14:06:34 +0100

    add logging to demo.py

--- tools/demo.py
+++ tools/demo.py
@@ -14,6 +14,10 @@ from glob import glob
 from pysot.core.config import cfg
 from pysot.models.model_builder import ModelBuilder
 from pysot.tracker.tracker_builder import build_tracker
+from pysot.utils.model_load import load_pretrain
+from pathlib import Path
+from datetime import datetime
+import logging
 
 torch.set_num_threads(1)
 
@@ -22,6 +26,8 @@ parser.add_argument('--config', type=str, help='config file')
 parser.add_argument('--snapshot', type=str, help='model name')
 parser.add_argument('--video_name', default='', type=str,
                     help='videos or image files')
+parser.add_argument('--logtofile', action='store_true', default=False, help="set if logging output to file")
+parser.add_argument('--initbbox', nargs='+', type=int, help='specify the initial bounding box')
 args = parser.parse_args()
 
 
@@ -59,39 +65,46 @@ def main():
     # load config
     cfg.merge_from_file(args.config)
     cfg.CUDA = torch.cuda.is_available() and cfg.CUDA
-    device = torch.device('cuda' if cfg.CUDA else 'cpu')
+    longterm_state = False
 
     # create model
     model = ModelBuilder()
 
     # load model
-    model.load_state_dict(torch.load(args.snapshot,
-        map_location=lambda storage, loc: storage.cpu()))
-    model.eval().to(device)
+    device = torch.device('cuda' if cfg.CUDA else 'cpu')
+    logger.info(f"Tracker running on {device}")
+    model = load_pretrain(model, args.snapshot).eval().to(device)
 
     # build tracker
     tracker = build_tracker(model)
 
     first_frame = True
     if args.video_name:
-        video_name = args.video_name.split('/')[-1].split('.')[0]
+        video_name = Path(args.video_name).stem
     else:
         video_name = 'webcam'
+    logger.info(f"Running demo on video '{video_name}'")
     cv2.namedWindow(video_name, cv2.WND_PROP_FULLSCREEN)
+    idx = 0
+    logger.info("bbox format: x (left corner), y (left corner), width, height")
     for frame in get_frames(args.video_name):
         if first_frame:
-            try:
-                init_rect = cv2.selectROI(video_name, frame, False, False)
-            except:
-                exit()
+            if args.initbbox:
+                init_rect = tuple(args.initbbox)
+            else:
+                try:
+                    init_rect = cv2.selectROI(video_name, frame, False, False)
+                except:
+                    exit()
             tracker.init(frame, init_rect)
+            logger.info(f"Initial bounding box: {init_rect}")
             first_frame = False
         else:
             outputs = tracker.track(frame)
             if 'polygon' in outputs:
                 polygon = np.array(outputs['polygon']).astype(np.int32)
                 cv2.polylines(frame, [polygon.reshape((-1, 1, 2))],
-                              True, (0, 255, 0), 3)
+                            True, (0, 255, 0), 3)
                 mask = ((outputs['mask'] > cfg.TRACK.MASK_THERSHOLD) * 255)
                 mask = mask.astype(np.uint8)
                 mask = np.stack([mask, mask*255, mask]).transpose(1, 2, 0)
@@ -99,11 +112,46 @@ def main():
             else:
                 bbox = list(map(int, outputs['bbox']))
                 cv2.rectangle(frame, (bbox[0], bbox[1]),
-                              (bbox[0]+bbox[2], bbox[1]+bbox[3]),
-                              (0, 255, 0), 3)
+                            (bbox[0]+bbox[2], bbox[1]+bbox[3]),
+                            (0, 255, 0), 3)
+            score = outputs['best_score']
+            cv2.putText(frame, "conficence: " + str(score), (40, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+            if cfg.TRACK.TYPE == "SiamRPNLTTracker":
+                if score < cfg.TRACK.CONFIDENCE_LOW:
+                    longterm_state = True
+                elif score > cfg.TRACK.CONFIDENCE_HIGH:
+                    longterm_state = False
+                cv2.putText(frame, "longterm state: " + str(longterm_state), (40, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
             cv2.imshow(video_name, frame)
+            logger.info(f"@ frame {idx}: confidence = {score}, bbox = {bbox}")
             cv2.waitKey(40)
+        idx += 1
 
 
 if __name__ == '__main__':
+    now = datetime.now()
+    now = now.strftime("%d_%m_%Y_%H_%M")
+
+    logging_dir_path = Path("../demo/logs")
+    if not os.path.isdir(logging_dir_path):
+        os.makedirs(logging_dir_path)
+
+    logger = logging.getLogger("global")
+    logger.setLevel(logging.INFO)
+
+    logging_path = Path(logging_dir_path, f"demo_log_{now}.log")
+    streamhandler = logging.StreamHandler()
+    FileFormatter = logging.Formatter(fmt=f"%(asctime)s %(name)s %(levelname)s: %(message)s", datefmt="%H:%M:%S")
+    streamhandler.setFormatter(FileFormatter)
+    
+    streamhandler.setLevel(logging.INFO)
+    if args.logtofile:
+        filehandler = logging.FileHandler(logging_path)
+        filehandler.setFormatter(FileFormatter)
+        filehandler.setLevel(logging.INFO)
+        logger.addHandler(filehandler)
+    logger.addHandler(streamhandler)
+
+    logger.info("Running demo")
+    logger.info("Given arguments: " + str(args))
     main()
